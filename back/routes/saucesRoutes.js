9d293ad975a879a6e05ff8110b0f2f8c0f3ea273
Mehdi <birkyo@gmail.com>
2022-10-09 13:10:49 +0200

    images folder

--- back/routes/saucesRoutes.js
+++ back/routes/saucesRoutes.js
@@ -5,7 +5,7 @@ const auth = require("../middleware/auth");
 const saucesCtrl = require("../controllers/saucesControllers.js");
 const multer = require("../middleware/multer-config.js");
 
-router.post("/", auth, multer, saucesCtrl.createSauce); // multer après auth sinon on peut modifier le fichier sans être authentifié
+router.post("/", auth, multer, saucesCtrl.createSauce); // auth > multer sinon on peut modifier le fichier sans être authentifié
 router.get("/", auth, saucesCtrl.getAllSauces);
 router.get("/:id", auth, saucesCtrl.getOneSauce);
 router.put("/:id", auth, multer, saucesCtrl.modifySauce);
